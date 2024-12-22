40fdefe5647d7dd3c24eea998e66d1334852299b
Brian Lee <bjhlee@gmail.com>
2024-12-21 19:19:51 -0500

    Update BOOTCAMP directory with recent modifications

---
+++ NYU_LeetCode_Bootcamp/Assignments/Week1_lists_arrays_sorting/Q1_LeetCode_217_contains_duplicate.py
@@ -0,0 +1,27 @@
+# LeetCode 217. Contains Duplicate
+# https://leetcode.com/problems/contains-duplicate/description/
+
+"""
+Given an integer array nums, 
+return true if any value appears at least twice in the array, and
+return false if every element is distinct.
+"""
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        set_elements = set()
+
+        for number in nums:
+            if number in set_elements:
+                return True
+            else:
+                set_elements.add(number)    
+
+        return False
+
+# testing
+nums = [1,2,3,1]
+
+solution = Solution()
+result = solution.containsDuplicate(nums)
+print(result)
